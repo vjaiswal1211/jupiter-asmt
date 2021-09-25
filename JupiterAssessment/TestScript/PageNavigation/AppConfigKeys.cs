53536afaf95d86301d608ceb599d06c5da24ac1a
Vishal Jaiswal <vishal.jaiswal@seera.com.au>
2021-09-25 15:38:22 +1000

    adding config keys

---
+++ JupiterAssessment/TestScript/PageNavigation/AppConfigKeys.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JupiterAssessment.Settings
+{
+   public class AppConfigKeys
+    {
+        public const string Browser = "Browser";
+
+        public const string Website = "Website";
+
+        public const string PageLoadTimeout = "PageLoadTimeout";
+
+        public const string ElementLoadTimeout = "ElementLoadTimeout";
+
+    }
+}
